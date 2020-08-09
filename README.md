b7c875fb1622887cfe21ef33fc1ead0149b22fc2
Denis <fsdvn1@gmail.com>
2020-08-09 22:03:12 +0500

    deployment script was added

--- README.md
+++ README.md
@@ -5,6 +5,28 @@ This project is inspired by [gcs-file-uploader](https://github.com/mcowger/gcs-f
 Scripts can get timed permission to upload files to some intermediate bucket and they don't need to
 store permissions or know anything about supporting infrastructure. One example is backup uploads. 
 
+## Environment variables
+To run locally or deploy to Cloud Run define environment variables or create `.env` file.
+```
+PORT=8080
+BUCKET=put-bucket-name-here
+SERVICE_NAME=signed-upload
+REGION=us-central1
+```
+
+## Running locally
+`docker.compose up --build`
+
+## Useful helper script
+It's possible to do everything using `gcloud` cli but `task.py` contains some useful
+shortcuts for common tasks. 
+
+```
+pip install -U python-dotenv invoke
+```
+
+`invoke deploy` - Deploy the latest version to managed Google Cloud Run.
+
 
 ## Why gcs service account key is built into the container?
 A better way would be to mount it when container starts but managed cloud run
--- tasks.py
+++ tasks.py
@@ -1,14 +1,36 @@
+import os
 from invoke import task
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_project_name(c):
-    result = c.run('gcloud config list --format="value(core.project)"', hide=True)
+def project_name(c):
+    result = c.run("gcloud config list --format='value(core.project)'", hide=True)
     return result.stdout.strip()
 
 
+def commit_sha(c):
+    result = c.run('git rev-parse --short HEAD')
+    return result.stdout.strip()
+
+
+def env(name):
+    return os.getenv(name)
+
+
 @task
-def build(c):
-    project_name = get_project_name(c)
-    print(project_name)
+def deploy(c, git_hash_tag=False):
+    tag = git_hash_tag and commit_sha(c) or 'latest'
+    image = f'gcr.io/{project_name(c)}/{env("SERVICE_NAME")}:{tag}'
+
+    c.run(f'docker build -t {image} ./')
+    c.run(f'docker push {image}')
+
+    c.run(
+        f'gcloud run deploy {env("SERVICE_NAME")} '
+        f'--image {image} --region {env("REGION")} '
+        f'--allow-unauthenticated --platform managed '
+        f'--update-env-vars '
+        f'BUCKET={env("BUCKET")}'
+    )
+
